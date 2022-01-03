eafad690b7839436c43877939ae4d9db60fca422
Jacco <jaccokiezebrink@gamil.com>
2022-01-03 19:21:12 +0100

    Fixed most issues, setup main loops and output csv writing

--- Report2/coreset.py
+++ Report2/coreset.py
@@ -1,5 +1,6 @@
 import math
 import random
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from pyspark.sql import SparkSession
@@ -14,9 +15,10 @@ class Coreset:
         self.grid = grid
 
     def compute_coreset(self, points, centers, sample_size=10):
-        coreset = np.array([[0, 0, 0]])
+        coreset = np.array([[0, 0, 0]])  # array containing coreset points which wil be returned
 
-        D = [np.sqrt(np.power(points[:, 0:2] - center, 2).sum(axis=1)) for center in centers]
+        # distance vector between each point and center
+        distances = [np.sqrt(np.power(points[:, 0:2] - center, 2).sum(axis=1)) for center in centers]
         cost = np.sum([point[2] * np.sqrt(np.sum(np.power(point[0:2] - centers, 2))) for point in points])
         levels = int(np.ceil(np.log(len(points)) * np.log(self.a * np.log(len(points)))))
         radius = np.sqrt(cost / (self.a * np.log(len(points)) * len(points)))
@@ -24,69 +26,73 @@ class Coreset:
         last_radius = 0
         current_radius = radius
         for i in range(levels):
+            # stop if there are no points left
             if len(points) == 0:
                 print("Stopping at level", i, "out of", levels)
                 break
 
+            # per level remove/sample points per center
             for c in range(len(centers)):
-                index = np.where((last_radius < D[c]) & (D[c] < current_radius))
+                # Get all points within current anulus
+                index = np.where((last_radius < distances[c]) & (distances[c] < current_radius))
                 annulus_subset = points[index[0]]
-                combined_sub = 0
 
                 # grid sampling
                 if self.grid:
-                    side_length = (self.eps * current_radius) / np.sqrt(2)
-                    grid_length = np.ceil((current_radius * 2) / side_length)
+                    side_length = (self.eps * current_radius) / np.sqrt(2)  # side length of each cell
+                    grid_length = np.ceil((current_radius * 2) / side_length)  # number of cells per axis
 
+                    # grid ranges
                     x_range = np.arange(centers[c][0] - (side_length * grid_length) / 2.0,
-                                        #np.ceil(centers[c][0] + (side_length * grid_length) / 2.0), side_length)
-                                        centers[c][0] + 1.0 + (side_length * (grid_length + 1)) / 2.0, side_length)
+                                        np.ceil(centers[c][0] + (side_length * grid_length) / 2.0), side_length)
                     y_range = np.arange(centers[c][1] - (side_length * grid_length) / 2.0,
-                                        #np.ceil(centers[c][1] + (side_length * grid_length) / 2.0), side_length)
-                                        centers[c][1] + (side_length * (grid_length + 1)) / 2.0, side_length)
-                    #if len(x_range) < grid_length - 1 or len(y_range) < grid_length - 1:
-                    #    print(grid_length, side_length, centers[c], x_range, y_range)
+                                        np.ceil(centers[c][1] + (side_length * grid_length) / 2.0), side_length)
 
+                    # loop over each cell
                     for x_i in range(len(x_range) - 1):
                         for y_i in range(len(y_range) - 1):
                             subset = annulus_subset[
                                 (annulus_subset[:, 0] > x_range[x_i]) & (annulus_subset[:, 0] < x_range[x_i + 1]) & (
                                         annulus_subset[:, 1] > y_range[y_i]) & (
                                             annulus_subset[:, 1] < y_range[y_i + 1])]
-                            combined_sub += len(subset)
                             if len(subset) != 0:
                                 sample_id = np.random.choice(subset.shape[0], 1, replace=False)
                                 coreset_point = subset[sample_id]
-                                coreset_point[0][2] = np.sum(subset[:, 2])
+                                coreset_point[0][2] = np.sum(subset[:, 2])  # set weight to sum of all points weights
                                 coreset = np.append(coreset, coreset_point, axis=0)
 
                 # annulus sampling
                 else:
                     sample_size_current = annulus_subset.shape[0] if sample_size > annulus_subset.shape[
                         0] else sample_size
-                    if sample_size_current == 0:
+                    if sample_size_current == 0:  # skip in case no points in annulus
                         continue
                     index_subset = np.random.choice(annulus_subset.shape[0], int(sample_size_current), replace=False)
                     subset = points[index_subset]
+                    # set weight to the sum of all points weights divided by the number of sampled points
                     subset[:, 2] = np.sum(annulus_subset[:, 2]) / sample_size_current
                     coreset = np.append(coreset, subset, axis=0)
 
                 # remove points from annulus subset in overall points set as they shouldn't be considered in other balls
                 points = np.delete(points, index[0], axis=0)
                 for c_id in range(len(centers)):
-                    D[c_id] = np.delete(D[c_id], index[0], axis=0)
+                    distances[c_id] = np.delete(distances[c_id], index[0], axis=0)
 
+            # increment radius
             last_radius = current_radius
             current_radius = np.power(2, i + 1) * radius
 
+        # add all points to the coreset which are left
+        coreset = np.append(coreset, points, axis=0)  # TODO: Check
+
         return coreset[1:]
 
-    def compute(self, points, sample_size=10):
+    def compute(self, points, seed=0, sample_size=10):
         points = points.astype(np.float32)  # ensure type is large enough as all points will be squared
         unweighted_points = points[:, 0:2]
 
         # compute centers according to kmeans++
-        centers = kmeans_plusplus(unweighted_points, self.k)[0]
+        centers = kmeans_plusplus(X=unweighted_points, n_clusters=self.k, random_state=seed)[0]
 
         # Comupte coreset
         coreset = self.compute_coreset(points, centers, sample_size)
@@ -97,8 +103,12 @@ class Coreset:
 
         return coreset
 
-    def mpc_compute(self, data, machines, seed, sample_size=10):
+    def mpc_compute(self, data, machines, seed=0, sample_size=10):
+        np.random.seed(self, seed=100)
         random.seed(seed)
+
+        round_timings = []
+        start_time = time.time()
 
         # split points
         point_sets = []
@@ -110,9 +120,6 @@ class Coreset:
             else:
                 point_sets[i % machines] = np.append(point_sets[i % machines], [points[i]], axis=0)
 
-        x = [np.sum(c[:, 2]) for c in point_sets]
-        print(sum(x), x)
-
         spark: SparkSession = SparkSession.builder \
             .master("local[" + str(machines) + "]") \
             .appName("SparkDB") \
@@ -120,17 +127,19 @@ class Coreset:
 
         iterations = 0
         while True:
+            round_start_time = time.time()
             iterations += 1
-            coreset_sets = spark.sparkContext.parallelize(point_sets).map(lambda x: self.compute(x, sample_size)).collect()
+            coreset_sets = spark.sparkContext.parallelize(point_sets) \
+                                                        .map(lambda x: self.compute(x, seed, sample_size)).collect()
 
             if len(coreset_sets) == 1:
                 print("Finished mpc coreset computation in", iterations, "rounds\nFinal coreset contains",
                       len(coreset_sets[0]), "points")
-                spark.stop()  # close session
-                return coreset_sets[0]
+                round_timings.append(round(time.time() - round_start_time, 2))
+                total_time = round(time.time() - start_time, 2)
 
-            x = [np.sum(c[:, 2]) for c in coreset_sets]
-            print(sum(x), x)
+                spark.stop()  # close session
+                return coreset_sets[0], total_time, round_timings
 
             point_sets = []
             for i in range(math.floor(len(coreset_sets) / 2)):
@@ -141,3 +150,5 @@ class Coreset:
             # If we have an odd amount of coreset sets, we union the last three
             if len(coreset_sets) % 2 != 0:
                 point_sets[-1] = np.append(point_sets[-1], coreset_sets[-1], axis=0)
+
+            round_timings.append(round(time.time() - round_start_time, 2))
--- Report2/main.py
+++ Report2/main.py
@@ -3,21 +3,56 @@ import coreset as agc
 import common.utils as utils
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from sklearn.cluster import KMeans
 
-data = input.parse_txt("dataset/s-set/s1.txt")
-data = np.append(data, np.ones(len(data)).reshape(len(data), 1), axis=1)
-opt = input.parse_txt("dataset/s-set/s1-label.pa")
-centers = input.parse_txt("dataset/s-set/s1-cb.txt")
+# Output data format: Grid or Ball sampling, sklearn dataset type, number of points in the dataset, seed used,
+#                      sample size for Ball sampling, total runtime, array of runtime per round/iterations,
+#                      number of points in the resulting coreset, quality of solution
+output_data = pd.DataFrame([], columns=["SamplingMethod", "DatasetType", "NrOfPoints", "Seed", "SampleSize", "TotalTime", "RoundTimes", "CoresetSize", "Quality"])
+row_counter = 0  # keep track of row in dataframe
 
-#Computing geometric decomposition coreset
-geo = agc.Coreset(15, 0.50, 0.5, True)
-coreset = geo.mpc_compute(data, 8, 0)
-print(len(coreset))
-print(coreset)
-print(np.sum(coreset[:,2]))
+epsilon = 1/16
+a = 0.5
+machines = 8
+seeds = [100, 101, 102, 103, 104]
+sample_sizes = []  # TODO: to be determined
 
+# TODO: dataset generation
+datasets = [] # or set of filenames
+
+for seed in seeds:
+    for dataset in datasets:
+        # assuming data only contains x and y, add weights of all ones
+        dataset = np.append(dataset, np.ones(len(dataset)).reshape(len(dataset), 1), axis=1)
+        centers = 0  # TODO: obtain number of centers/clusters for each dataset
+
+        # TODO: apply kmeans on original dataset
+
+        # Run grid sampling
+        geo = agc.Coreset(centers, epsilon, a, True)
+        coreset, total_time, round_times = geo.mpc_compute(dataset, machines, seed)
+        # TODO: - Run kmeans on coreset
+        #       - Compare quality against original kmeans centroid position
+        output_data.loc[row_counter] = ["Grid", "dataset type", "n", seed, 0, total_time, round_times, len(coreset),
+                                        "quality"]  # TODO: Fill dataset type, n and quality variables
+        output_data.to_csv("output_data.csv", index=False)
+        row_counter += 1
+
+        for sample_size in sample_sizes:
+            # Run ball sampling
+            geo = agc.Coreset(centers, epsilon, a, False)
+            coreset, total_time, round_times = geo.mpc_compute(dataset, machines, seed)
+            # TODO: - Run kmeans on coreset
+            #       - Compare quality against original kmeans centroid position
+            output_data.loc[row_counter] = ["Grid", "dataset type", "n", seed, 0, total_time, round_times, len(coreset)
+                                            "quality"]  # TODO: Fill dataset type, n and quality variables
+            output_data.to_csv("output_data.csv", index=False)
+            row_counter += 1
+
+
+'''
 @utils.timeit
 def test_no_coreset():
     kmeans = KMeans(n_clusters=15, random_state = 0).fit(X=data)
@@ -33,11 +68,12 @@ def test_coreset():
     return cost
 
 
-#cost = test_no_coreset()
-#cost_cs = test_coreset()
-#cost_opt = utils.cost_function(data, opt, centers)
+cost = test_no_coreset()
+cost_cs = test_coreset()
+cost_opt = utils.cost_function(data, opt, centers)
 
-#print("cost no coreset ", cost)
-#print("cost coreset ", cost_cs)
-#print("coreset improvment: {:.1%} ".format(np.abs(cost-cost_cs)/cost))
+print("cost no coreset ", cost)
+print("cost coreset ", cost_cs)
+print("coreset improvment: {:.1%} ".format(np.abs(cost-cost_cs)/cost))
 plt.show()
+'''
